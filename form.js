834759a8f0fe3366ad4d5ac1f2dd6def0c353099
DionelExe <dionel412@gmail.com>
2023-03-24 19:38:00 -0300

    Initial commit

---
+++ form.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    // Envía el formulario
+    document.getElementById("myForm").submit();
+  
+    // Vacía el formulario
+    document.getElementById("myForm").reset();
+  
+    // Muestra una ventana emergente
+    alert("El formulario se ha enviado exitosamente.");
+  }
+  
